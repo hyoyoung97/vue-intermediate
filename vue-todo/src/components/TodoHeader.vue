c80623a286083c49d5c13e0760eb5602983b591d
Ayoung Kim <hyoyoung97@naver.com>
2022-03-11 17:21:44 +0900

    컴포넌트 생성, 파비콘, 아이콘, 폰트, 반응형 태그 설정하기

---
+++ vue-todo/src/components/TodoHeader.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>TODO it!</h1>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "TodoHeader"
+}
+</script>
+
+<!--scoped: 해당 컴포넌트 안에서만 유효한 스타일 속성 부모자식간의 영향을 받지 않음-->
+<style scoped>
+h1 {
+  color: #2F3B52;
+  font-weight: 900;
+  /*rem: */
+  margin: 2.5rem 0 1.5rem;
+}
+</style>
