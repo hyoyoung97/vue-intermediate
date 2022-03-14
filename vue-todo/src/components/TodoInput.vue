f0e3d19734375fe47f3e1aa81c84158d6d380b22
Ayoung Kim <hyoyoung97@naver.com>
2022-03-14 16:11:00 +0900

    TodoInput, TodoList 컴포넌트 구현

--- vue-todo/src/components/TodoInput.vue
+++ vue-todo/src/components/TodoInput.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodoItem">
-    <button @click="addTodo">add</button>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" @keyup.enter="addTodo">
+<!--    <button @click="addTodo">add</button>-->
+    <span class="addContainer" @click="addTodo">
+      <i class="fa-solid fa-plus addBtn"></i>
+    </span>
   </div>
 </template>
 
@@ -14,7 +17,16 @@ export default {
   },
   methods: {
     addTodo: function () {
-      localStorage.setItem(this.newTodoItem, this.newTodoItem); //개발자도구-애플리케이션에서 확인 가능함
+      if (this.newTodoItem !== '') {
+        var obj = {completed: false, item: this.newTodoItem};
+        //js 객체를 string으로 변환해주는 값
+        localStorage.setItem(this.newTodoItem, JSON.stringify(obj));  // Value > {completed: false, item: this.newTodoItem}
+        // localStorage.setItem(this.newTodoItem, obj); // Value > [object Object]
+        // localStorage.setItem(this.newTodoItem, this.newTodoItem); //개발자도구-애플리케이션에서 확인 가능함
+        this.clearInput();
+      }
+    },
+    clearInput: function () {
       this.newTodoItem = '';
     }
   }
@@ -22,5 +34,28 @@ export default {
 </script>
 
 <style scoped>
-
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+vertical-align: middle;
+}
 </style>
--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,15 +1,85 @@
 <template>
   <div>
-    list
+    <ul>
+      <li v-for="(todoItem, index) in todoItems" :key="todoItem.item" class="shadow">
+        <i class="checkBtn fa-solid fa-check" :class="{checkBtnCompleted: todoItem.completed}" @click="toggleComplete(todoItem)"></i>
+        <span :class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
+        <span class="removeBtn" @click="removeTodo((todoItem, index))">
+          <i class="fa-solid fa-trash-can"></i>
+        </span>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
-  name: "TodoList"
+  data: function () {
+    return {
+      todoItems: []
+    }
+  },
+  created: function () {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        // this.todoItems[i] = localStorage.key(i);
+        // this.todoItems.push(localStorage.key(i));
+        console.log(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+      }
+    }
+  },
+  methods: {
+    removeTodo: function (todoItem, index) {
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);  //splice - 특정 index에서 원하는 개수만큼 지울 수 있음, 기존 배열을 변경해서 새로운 배열을 만들어줌
+    },
+    toggleComplete: function (todoItem) {
+      todoItem.completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    }
+  }
 }
 </script>
 
 <style scoped>
+ul {
+  list-style-type: none;
+  padding-left: 0px;
+  margin-top: 0;
+  text-align: left;
+}
+
+li {
+  display: flex;
+  min-height: 50px;
+  height: 50px;
+  line-height: 50px;
+  margin: 0.5rem 0;
+  padding: 0 0.9rem;
+  background: white;
+  border-radius: 5px;
+}
+
+.removeBtn {
+  margin-left: auto;
+  color: #de4343;
+}
+
+.checkBtn {
+  line-height: 45px;
+  color: #62acde;
+  margin-right: 5px;
+}
+
+.checkBtnCompleted {
+  color: #b3adad;
+}
+
+.textCompleted {
+  text-decoration: line-through;
+  color: #b3adad;
+}
 
 </style>
